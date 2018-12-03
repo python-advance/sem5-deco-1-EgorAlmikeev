e007fcc887948ef184412e36e02d8b8eec27e173
EgorAlmikeev <ealmikeew@gmail.com>
2018-12-03 23:08:15 +0300

    Create app.py

---
+++ self/По выбору/app.py
@@ -0,0 +1,5 @@
+def logger(func):
+    import datetime
+    with open("log.txt", "a") as file:
+        file.write("{0:%Y-%m-%d %H:%M:%S}".format(datetime.datetime.now()) + " : " + func.__name__ + "\n")
+    return func
