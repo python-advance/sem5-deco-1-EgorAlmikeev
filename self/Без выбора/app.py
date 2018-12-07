2e0f39e4cf12975eac6045063c7cf10ceca19925
EgorAlmikeev <ealmikeew@gmail.com>
2018-12-07 21:06:24 +0300

    Update app.py

--- self/Без выбора/app.py
+++ self/Без выбора/app.py
@@ -1,33 +1,52 @@
-def logger(func):
+def logger(f):
     import functools
-    @functools.wraps(func)
-    def wrapper(*args, **kwargs):
-        result = func(*args, **kwargs)
-        with open("./log.txt", 'a') as file:
-            file.write("=" * 20)
-            file.write("\n")
-            file.write("args: " + str(args))
-            file.write("\n")
-            file.write("result: " + str(result))
-            file.write("\n")
+    @functools.wraps(f)
+    def wrapper(*args):
+        result = f(*args)
+        with open("./log.txt", "a") as file:
+            file.write(("-" * 20) + "\n")
+            file.write("Rubles: " + str(args[0]) + "\n")
+            file.write("Currency: " + str(args[2]) + "\n")
+            file.write("Result: " + str(result) + "\n")
         return result
     return wrapper
 
 
-@logger
-def make_calcs(operation, arg1, arg2):
-    result = None
-    if operation == "+":
-        result = arg1 + arg2
-    elif operation == "-":
-        result = arg1 - arg2
-    elif operation == "*":
-        result = arg1 * arg2
-    elif operation == "/":
-        result = arg1 / arg2
+def get_currencies():
+    response = ET.parse(urllib.request.urlopen("http://www.cbr.ru/scripts/XML_daily.asp"))
+    currencies = {}
+    for line in response.findall('Valute'):
+        currencies.update({line.find('CharCode').text: line.find('Value').text})
+    return currencies
 
-    return result
+
+def get_currency_value(currencies: dict, currencty_char):
+    return float(currencies[currencty_char].replace(",", "."))
+
+
+@logger
+def rubles_to_currency(rubles, currencies, currency_char):
+    return rubles / get_currency_value(currencies, currency_char)
 
 
 if __name__ == "__main__":
-    print(make_calcs("+", 1, 2))
+    print("Available currencies:")
+    currencies = get_currencies()
+    currency_chars = list(get_currencies().keys())
+
+    for (i, j) in enumerate(currency_chars):
+        print(i + 1, j, sep=": ")
+
+    while True:
+        try:
+            currency_char = currency_chars[(int(input("set: "))) - 1]
+        except IndexError:
+            print("No such currency")
+            continue
+        except TypeError:
+            print("Are you okay?")
+            continue
+        break
+
+    rubles = int(input("Rubles: "))
+    print(rubles, "RUB =", rubles_to_currency(rubles, currencies, currency_char), currency_char)
